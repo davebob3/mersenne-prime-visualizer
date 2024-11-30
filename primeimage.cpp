7a744d3b650a9a0d0468c66d0e9b7a9937641e9e
David Robson <davebob3@gmail.com>
2024-11-29 20:29:44 -0800

    Allow a single command line argument to be a numeral exponent.

--- primeimage.cpp
+++ primeimage.cpp
@@ -7,18 +7,65 @@
 //
 
 #include <iostream>
+#include <string>
+
 #include "CImg/CImg.h"
 
 using namespace cimg_library;
 
+static int get_exponent(const char *argument, unsigned long *value)
+{
+    int result = 0;
+    unsigned long exponent_value = 0;
+    int arg_size = std::strlen(argument);
+    for (int c = 0; c < arg_size; c++)
+    {
+        if (!std::isdigit(argument[c]))
+        {
+            result = __LINE__;
+            break;
+        }
+    }
+    if (result == 0)
+    {
+        try 
+        {
+            exponent_value = std::stoul(argument);
+        }
+        catch (...)
+        {
+            result = __LINE__;
+        }
+    }
+    if (result == 0)
+    {
+        *value = exponent_value;
+    }
+    return result;
+}
+
 int main(int argc, const char *argv[])
 {
-    unsigned int thePrimeFactor = 136279841;
-    unsigned int theImageWidth = 400;
-    unsigned int theImageHeight = 300;
-    unsigned int theIncrement = 100;
-    unsigned int theNumberWhitePix = thePrimeFactor / 24;
-    unsigned int theRemainderNum = thePrimeFactor % 24;
+    unsigned long thePrimeFactor = 136279841;
+    unsigned long theImageWidth = 400;
+    unsigned long theImageHeight = 300;
+    unsigned long theIncrement = 100;
+
+    if (argc == 2)
+    {
+        unsigned long argument;
+        if (get_exponent(argv[1], &argument) != 0)
+        {
+            std::cout << "Could not parse number from commandline, using exponent "
+                << thePrimeFactor << std::endl;
+        }
+        else
+        {
+            thePrimeFactor = argument;
+        }   
+    }
+    unsigned long theNumberWhitePix = thePrimeFactor / 24;
+    unsigned long theRemainderNum = thePrimeFactor % 24;
     uint32_t theRemainder = 0;
     for (unsigned int bit = 0; bit < theRemainderNum; bit++)
     {
@@ -37,20 +84,13 @@ int main(int argc, const char *argv[])
     unsigned char remainder[] = {(unsigned char)(theRemainder & 0xff),
                                  (unsigned char)((theRemainder >> 8) & 0xff),
                                  (unsigned char)((theRemainder >> 16) & 0xff)};
-    std::cout << "Width: " << theImageWidth << std::endl;
-    std::cout << "Height: " << theImageHeight << std::endl;
-    std::cout << "Size: " << theImageHeight * theImageWidth << std::endl;
-    std::cout << "Whitepix: " << theNumberWhitePix << std::endl;
+    std::cout << "rendering 2^" << thePrimeFactor << " - 1" << std::endl;
+    std::cout << "Width: " << theImageWidth << ", Height: " << theImageHeight << std::endl;
+    std::cout << "Size: " << theImageHeight * theImageWidth << ", White pixels: " << theNumberWhitePix << std::endl;
     std::cout << "Remainder: " << std::hex << theRemainder << std::endl;
-
-    for (int c = 0; c < 3; c++)
-    {
-        std::cout << (int)remainder[c] << std::endl;
-    }
 
     CImg<unsigned char> image(theImageWidth, theImageHeight, 1, 3);
 
-    cimg::info();
     unsigned int currentLocation = 0;
     for (unsigned int h = 0; h < theImageHeight; h++)
     {
